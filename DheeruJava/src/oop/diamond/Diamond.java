30e90338b89e68f3eb5ce5a109c2f52b5f92e625
Aleks Grig <ullitap@gmail.com>
2020-01-19 21:25:21 +0300

    Diamond problem.

---
+++ DheeruJava/src/oop/diamond/Diamond.java
@@ -0,0 +1,18 @@
+package oop.diamond;
+
+// If both interfaces have same default method we must override it in the class
+public class Diamond implements I1, I2 {
+
+	@Override
+	public void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DheeruJava/src/oop/diamond/I2.java
@@ -0,0 +1,9 @@
+package oop.diamond;
+
+public interface I2 {
+	public void method3();
+
+//	default void method2() {
+//		System.out.println("Interface2 - default method2()");
+//	}
+}
