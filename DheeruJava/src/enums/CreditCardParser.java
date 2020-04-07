66c5d412f0b39dc4007ec65ef072d45f7b2b536a
Aleks Grig <ullitap@gmail.com>
2020-04-07 15:38:25 +0300

    CreditCardParser refactoring

--- DheeruJava/src/enums/CreditCardParser.java
+++ DheeruJava/src/enums/CreditCardParser.java
@@ -3,10 +3,11 @@ package enums;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CreditCardParser {
 
-	private enum CreditCard
+	public enum CreditCard
 	{
 		VISA(List.of("4"), Set.of(16)),
 		Amex(List.of("34", "37"), Set.of(15)),
@@ -21,8 +22,7 @@ public class CreditCardParser {
 			this.prefixes = prefixes;
 		}
 
-		public boolean instanceOf(String code) {
-			code = code.replace(" ", "");
+		private boolean instanceOf(String code) {
 			if(lengths.contains(code.length())) {
 				for(String prefix : prefixes) {
 					if(code.startsWith(prefix)) {
@@ -32,19 +32,22 @@ public class CreditCardParser {
 			}
 			return false;
 		}
+		
+		public static String checkCardType(String code) {
+			var result =  Arrays.asList(CreditCard.values()).stream()
+				.filter(card -> card.instanceOf(code.replace(" ", "")))
+				.findFirst()
+				.map(card -> card.name());
+			return result.isPresent() ? result.get() : "Match not found";
+		}
 	}
 
 	public static void main(String[] args) {
-		System.out.println(checkCardType("4544 4444 4444 4444"));
-	}
-
-	private static String checkCardType(String code) {
-		var result =  Arrays.asList(CreditCard.values())
-			.stream()
-			.filter(card -> card.instanceOf(code))
-			.findFirst()
-			.map(card -> card.name());
-		return result.isPresent() ? result.get() : "Match not found";
+		var creditCards = List.of("6544 4444 4444 4444", "4444 4444 4444 4444", 
+				"6044 4444 4444 4444", "3444 4444 4444 444", "3744 4444 4444 4444", "3544 4444 4444 4444 444");
+		var cardTypes = creditCards.stream()
+			.collect(Collectors.toMap(card -> card, card -> CreditCard.checkCardType(card)));
+		System.out.println(cardTypes);
 	}
 
 }
